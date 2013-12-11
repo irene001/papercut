cc569eb7ac4dd6e0cdfd159e23ed27cccde50b38
Alexander.ivanov <alexander.ivanov@teamdev.com>
2013-12-11 15:26:26 +0200

    textarea added instead of input

--- models/Message.js
+++ models/Message.js
@@ -6,13 +6,33 @@ client.on("error", function (err) {
 });
 
 exports.save = function(msg){
-    client.set("test_key", msg);// todo: generate id automatically
-}
+    client.set('test_key', msg);
+};
+
+exports.load = function(key, callback) {
+    client.unref();
+    client.get('test_key', function (err, value){
+        if (err) throw(err);
+        console.log("messages: data from database "+value);
+        callback(value);
+        convert(value);
+    });
+};
 
 exports.find = function(id){// todo: choose correct implementation
-    client.unref()//todo: check it out in documentation
+    client.unref();//todo: check it out in documentation
     client.get(id, function (err, value){
-        if (err) throw(err)
+        if (err) throw(err);
         return value;
     });
-}
+};
+
+convert = function(message) {
+    console.log("first symbol: ", message.charAt(0));
+    console.log("message length: ", message.length);
+    for (var symbol in message) {
+        console.log("symbol "+symbol+"equals to image "+symbol+"_image");
+
+    }
+
+};
--- routes/papercut.js
+++ routes/papercut.js
@@ -2,19 +2,26 @@ var msgModel = require('./../models/Message');
 
 exports.index = function(req, res){
     console.log("index page");
-  res.render('index', { title: 'Papercut' });
+    res.render('index', { title: 'Papercut' , div: "Corrected message:", pic: 'images/default.jpg'});
 };
 
 exports.open = function(req, res) {
     msgModel.find(req.params.id);
     res.render("papercut.html");// todo: open correct screen
-}
+};
 
 exports.generate = function(req, res) {
+
     console.log("generate image");
     var msg = req.body.message;
     msgModel.save(msg);
-    res.redirect('/');// TODO: we should generate link, display it on the index page(on the current page)
-}
+    msgModel.load('test_key', function(value){
+        res.render('index', { title: 'Papercut' , div: "Corrected message: " + value, pic: 'images/e.jpg'});
+    });
+};
+
+
+
+
 
 
