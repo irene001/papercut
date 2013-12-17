399f173e853e48c4da641642d82c56c885ff67c8
Irene Prys <prysirene@gmail.com>
2013-12-17 14:33:14 +0200

    using the async module; a little code refactoring in the papercut.js;

--- routes/papercut.js
+++ routes/papercut.js
@@ -1,40 +1,62 @@
 var msgModel = require('./../models/Message');
 var config = require('./../config');
+var async = require('async');
+
+var UNDEFINED_LETTER_PATH = "undefined1.png"
 
 exports.index = function(req, res){
     console.log("index page");
     res.render('index', { title: 'Papercut' , div: "Corrected message:", pic: 'images/default.jpg'});
 };
 
-exports.openMessage = function(req, res) {
-    var count = 0;
-    var images = [];
-    msgModel.find(req.params.id, function (err, msg){
-        for (var i = 0; i < msg.length; i++) {
-            //todo: handle 'enter' //todo: handle not found // todo: handle dot and space
-            var letter = msg.charAt(i);
-            msgModel.findImgPath(letter === ' '? 'space' : letter, function(err, letterPath){
-                if (!letterPath) {
-                    letterPath = "undefined1.png";
-                }
-                console.log("images: " + config.get('imageHost') + letterPath);
-                images.push(config.get('imageHost') + letterPath);
-                count++;
-                if (count == msg.length) {// todo: fix it
-                    res.render('canvas', { title: 'Message' , sources: images});
-                }
-            });
-        }
-    });
-}
-
-exports.saveMessage = function(req, res) {
+exports.saveMessage = function (req, res) {
     console.log("generate image");
     var msg = req.body.message;
-    msgModel.save(msg, function(key){
+    msgModel.save(msg, function (key) {
         res.redirect('/papercut/' + key);
     });
 };
+
+exports.openMessage = function (req, res) {
+    var count = 0;
+    var images = [];
+
+    msgModel.find(req.params.id, parseMessage);
+
+    function parseMessage(err, msg) {
+        var callbacks = [];
+
+        for (var i = 0; i < msg.length; i++) {
+            var letter = msg.charAt(i);
+            callbacks.push(searchByLetter(letter));
+        }
+
+        function searchByLetter(letter) {
+            return function (callback) {
+                msgModel.findImgPath(letter === ' ' ? 'space' : letter, processLetter(callback));
+            }
+        }
+
+        async.parallel(callbacks, function (err) {
+            if (err) {
+                throw err;
+            }
+            res.render('canvas', { title: 'Message', sources: images});
+        });
+    }
+
+    function processLetter(callback) {
+        return function (err, letterPath) {
+            if (!letterPath) {
+                letterPath = UNDEFINED_LETTER_PATH;
+            }
+            console.log("images: " + config.get('imageHost') + letterPath);
+            images.push(config.get('imageHost') + letterPath);
+            callback();
+        }
+    }
+}
+
 
 
 
